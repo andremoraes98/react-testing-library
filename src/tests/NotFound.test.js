e1ccd67dfa237b9758eb19bf137ed381a2eaab6b
André Luis Oliveira Rocha de Moraes <andre.moraes.98@gmail.com>
2022-03-22 16:20:01 -0300

    Cumprimento do requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente "<NotFound />".', () => {
+  it('verifica se a página contém um heading H2 com um texto específico.', () => {
+    render(<NotFound />);
+
+    const notFoundTitle = screen
+      .getByRole('heading', { name: /Page requested not found/i, level: 2 });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  it('verifica se a página contém uma imagem específica.', () => {
+    render(<NotFound />);
+
+    const URL_IMAGE = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const notFoundImage = screen.getAllByRole('img');
+
+    expect(notFoundImage[1]).toHaveAttribute('src', URL_IMAGE);
+  });
+});
